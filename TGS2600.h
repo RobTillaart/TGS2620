280896db5c5c42fbe6a3b8eac79d66e2ef2ca4d4
Rob Tillaart <rob.tillaart@gmail.com>
2026-02-11 16:04:33 +0100

    initial version

---
+++ TGS2600.h
@@ -0,0 +1,9 @@
+
+
+//  https://forum.arduino.cc/t/figaro-tgs2620-sensor-calibration/1430074/2
+
+//  https://github.com/zigpot/tgs2600-on-arduino
+//  https://zigpot.wordpress.com/2021/09/29/setting-up-gas-sensor-for-microcontroller/
+
+
+  
